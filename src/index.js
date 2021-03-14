a12e4811bed01ecfac8930373607cf673d241789
casper-ilona <hair.masterpiece@gmail.com>
2021-03-14 22:52:03 +0300

    solved problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    let minValue = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (minValue > array[i]) {
+            minValue = array[i];
+        }
+        
+    }
+    return minValue;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+            return 0;
+        }
+    return  Math.max(...array);
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    return array.reduce((a,b)=>(a+b))/array.length;
+};
